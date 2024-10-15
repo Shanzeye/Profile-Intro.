26b950779b95a2f9dfcb888188a4b7e8a458cd9e
Shanzeye <165150162+Shanzeye@users.noreply.github.com>
2024-10-15 18:08:53 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,29 @@
+ 
+
+ export default function () {
+  return (
+    
+    <section className="text-gray-600 body-font overflow-hidden">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="lg:w-4/5 mx-auto flex flex-wrap">
+      <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+        <h2 className="text-sm title-font text-gray-500 tracking-widest">PROFILE INTRODUCTION.</h2>
+        <h1 className="text-gray-900 text-3xl title-font font-medium mb-4">HELLO EVERYONE! </h1>
+        <div className="flex mb-4">
+          <a className="flex-grow text-indigo-500 border-b-2 border-indigo-500 py-2 text-lg px-1">Description</a>
+          
+        </div>
+        <p className="leading-relaxed mb-4 text text-2xl"> "I am a student of Governor institute of Artificial Intelligence & Computing" Apart from that i am also a Graphic Designer &  Wordpress developer.</p>
+        
+        
+      </div>
+      <img alt="ecommerce" className="lg:w-1/3 w-full lg:h-auto h-64 object-cover object-center rounded" src="https://m.media-amazon.com/images/I/811WvXGGgIL._h1_.png"/>
+    </div>
+  </div>
+</section>
+  )
+}
+
+
+
+
